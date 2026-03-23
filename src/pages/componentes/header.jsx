156d01f3a430b525d00967c24e8a963c655cb9ac
Carlos Davi <carlosdavi1314@gmail.com>
2026-03-23 19:02:46 -0300

    Corrigir nome

--- src/pages/componentes/header.jsx
+++ src/pages/componentes/header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return ( 
 <header class="fixed top-0 left-0 w-full z-50 bg-gradient-to-b from-[#19191c] to-transparent flex justify-between items-center px-6 py-4 w-full">
-<h1 class="font-['Plus_Jakarta_Sans'] tracking-tight leading-tight uppercase font-bold text-xl tracking-tighter text-[#a1faff]">Soud Music</h1>
+<h1 class="font-['Plus_Jakarta_Sans'] tracking-tight leading-tight uppercase font-bold text-xl tracking-tighter text-[#a1faff]">Sound Music</h1>
 </header>
      );
 }
